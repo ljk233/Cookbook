8a29b8b7f15c2245a2db2c74ecb0d94ca3e10002
Liam Kelly <liam.james.kelly@gmail.com>
2023-07-27 15:30:58 +0100

    Removed steps in docstring and update DF interface

--- Probability/plot_scipy_rv_discrete.py
+++ Probability/plot_scipy_rv_discrete.py
@@ -1,8 +1,4 @@
-"""Plot a SciPy discrete random variable as a bar plot.
-
-1. Generate the domain
-2. Initialise a pandas DataFrame and pipe it into sns.catplot
-"""
+"""Plot a SciPy discrete random variable as a bar plot."""
 
 # %%
 import numpy as np
@@ -17,6 +13,6 @@ rv = st.binom(n=20, p=0.5)
 
 # Plot the random variable
 x = np.arange(rv.ppf(0.01), rv.ppf(0.99) + 1, dtype=int)
-pd.DataFrame(data={"X": x, "PX": rv.pmf(x)}).pipe(
-    sns.catplot, x="X", y="PX", kind="bar", color="cornflowerblue"
+pd.DataFrame(data={"X": x, "p": rv.pmf(x)}).pipe(
+    sns.catplot, x="X", y="p", kind="bar", color="cornflowerblue"
 )
