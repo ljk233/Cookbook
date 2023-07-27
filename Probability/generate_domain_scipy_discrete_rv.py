05bc04c13ff19c2e6041f6edeec00cfb005d0c48
Liam Kelly <liam.james.kelly@gmail.com>
2023-07-27 15:34:10 +0100

    Updated to mirror domain cont_rv

--- Probability/generate_domain_scipy_discrete_rv.py
+++ Probability/generate_domain_scipy_discrete_rv.py
@@ -2,11 +2,15 @@
 
 # %%
 import numpy as np
+import pandas as pd
 from scipy import stats as st
 
 
 # Initialise the random variable
 rv = st.binom(n=20, p=0.5)
 
+# Return the domain as a np.array
+x = np.arange(rv.ppf(0.01), rv.ppf(0.99) + 1, dtype=int)
+
 # Return the domain as a pandas Series
-np.arange(rv.ppf(0.01), rv.ppf(0.99) + 1, dtype=int)
+x_se = pd.Series(x, name="X")
