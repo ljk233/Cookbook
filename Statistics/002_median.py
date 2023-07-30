4a65ddf5e1c51978b420e8cf4e46666440697a25
Liam Kelly <liam.james.kelly@gmail.com>
2023-07-30 06:39:15 +0100

    Create 002_median.py

---
+++ Statistics/002_median.py
@@ -0,0 +1,17 @@
+# %%
+"""Return the median of a dataset."""
+
+import pandas as pd
+import statsmodels.api as sm
+
+# ======================================================================
+# Get things ready
+# ======================================================================
+
+X: pd.Series = sm.datasets.get_rdataset("Bwages", "Ecdat").data["wage"]
+
+# ======================================================================
+# Return the median
+# ======================================================================
+
+X.quantile(0.5)
