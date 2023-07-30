40192790cae9ca5182ba067d90f79dfc1731f316
Liam Kelly <liam.james.kelly@gmail.com>
2023-07-30 06:41:47 +0100

    Create 010_percentile.py

---
+++ Statistics/010_percentile.py
@@ -0,0 +1,17 @@
+# %%
+"""Return the Nth percentile of a dataset."""
+
+import pandas as pd
+import statsmodels.api as sm
+
+# ======================================================================
+# Get things ready
+# ======================================================================
+
+X: pd.Series = sm.datasets.get_rdataset("Bwages", "Ecdat").data["wage"]
+
+# ======================================================================
+# Return the Nth percentile
+# ======================================================================
+
+X.quantile(0.05)
