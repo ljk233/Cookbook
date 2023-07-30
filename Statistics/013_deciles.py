a113aba2ec50790253985fcbe4ff56ea1a538f3c
Liam Kelly <liam.james.kelly@gmail.com>
2023-07-30 06:43:00 +0100

    Create 013_deciles.py

---
+++ Statistics/013_deciles.py
@@ -0,0 +1,18 @@
+# %%
+"""Return the deciles of a dataset."""
+
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+
+# ======================================================================
+# Get things ready
+# ======================================================================
+
+X: pd.Series = sm.datasets.get_rdataset("Bwages", "Ecdat").data["wage"]
+
+# ======================================================================
+# Return the Nth percentile
+# ======================================================================
+
+X.quantile(np.linspace(0.1, 1, num=10)).rename("deciles")
